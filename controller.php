e69cdfc5a97b0cab361161b7ffa615a8fb70185d
neglitis <niklavs.eglitis@esynergy.lv>
2018-11-29 19:06:54 +0200

    Revision edit

--- controller.php
+++ controller.php
@@ -1,14 +1,23 @@
 <?php
-var_dump($_GET);
-var_dump($_POST);
+
+$debug = false;
+
+if ($debug) {
+    var_dump($_GET);
+    var_dump($_POST);
+    var_dump($_SERVER);
+}
 
 $controller = (isset($_GET['controller'])) ? $_GET['controller'] : 'product';
 $action = (isset($_GET['action'])) ? $_GET['action'] : null;
 $id = (isset($_GET['id'])) ? $_GET['id'] : null;
 
-echo $id;
-echo $action;
-echo $controller;
+if ($debug) {
+    echo $id;
+    echo $action;
+    echo $controller;
+}
+
 include('functions.php');
 
 switch ($controller) {
@@ -21,10 +30,11 @@ switch ($controller) {
                 header("Location:http://localhost:8000/product-edit.php?id=".$id);
                 exit();
             case 'delete':
-                delete_record($id);
-                header("Location:http://localhost:8000/product-list.php");
-                exit();
-            case 'list':
+                if (isset($_POST['action']) && $_POST['action'] == 'delete') {
+                    if (isset($_POST['id']) && is_int($_POST['id'])) {
+                        delete_record($id);
+                    }
+                }
                 header("Location:http://localhost:8000/product-list.php");
                 exit();
             case 'add':
@@ -32,6 +42,10 @@ switch ($controller) {
                 exit();
             case 'save':
                 header("Location:http://localhost:8080/product-edit.php");
+                exit();
+            case 'list':
+            default:
+                header("Location:http://localhost:8000/product-list.php");
                 exit();
         }
         // no break
